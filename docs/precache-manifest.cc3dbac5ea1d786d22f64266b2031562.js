e15193e090f3bb8ff672037de0f8905abec20846
Carlos Brignardello <carlos.alb.brig@gmail.com>
2020-07-14 21:06:09 -0400

    se agrega el proyecto

---
+++ docs/precache-manifest.cc3dbac5ea1d786d22f64266b2031562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f44c5df2d06ffe9df3d6a6e4f2ee81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be2e227a58c8d19b6cff",
+    "url": "/static/css/main.2ca97680.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2e227a58c8d19b6cff",
+    "url": "/static/js/main.dee40f8a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
